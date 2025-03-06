9704c93f0d570b46e5b5b9b0688a2daca45aaca5
1_cryptoman.lens <114017015+titusko@users.noreply.github.com>
2025-03-06 14:41:33 +0000

    gg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,8 @@
-
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { WalletProvider } from '../src/providers/WalletProvider';
-import { Web3Provider } from '../src/providers/Web3Provider';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-// Create a client
-const queryClient = new QueryClient();
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <WalletProvider>
-        <Web3Provider>
-          <Component {...pageProps} />
-        </Web3Provider>
-      </WalletProvider>
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
